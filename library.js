354073b87ff084e8ea0ad3fd6fe33373018eb89c
Konsta Nenonen <konstanenonen@protonmail.com>
2022-04-11 12:38:26 +0300

    Done with the basic layout of the app. Also added addBookToLibrary function

--- library.js
+++ library.js
@@ -1,3 +1,5 @@
+const myLibrary = [];
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -8,4 +10,16 @@ Book.prototype.info = function () {
   return `${this.title} by ${this.author}, ${this.pages} pages, ${
     this.read ? "already read" : "not read yet"
   }`;
+}
+
+function addBookToLibrary(array) {
+  array.forEach(book => {
+    const bookElement = document.createElement("div");
+    const bookInfo = document.createElement("p");
+
+    bookInfo.innerText = book.info();
+    bookElement.appendChild(bookInfo);
+
+    document.querySelector(".main").appendChild(bookElement);
+  });
 }
