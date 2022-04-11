28f247a7ef7b4e5a2b9ae2b2ed0e5c2f382aec00
Konsta Nenonen <konstanenonen@protonmail.com>
2022-04-11 13:30:09 +0300

    Can now add new book element to main area with the book form

--- library.js
+++ library.js
@@ -1,5 +1,14 @@
 const myLibrary = [];
 
+document
+  .querySelector(".show-book-form-btn")
+  .addEventListener("click", toggleForm);
+
+document.querySelector(".book-form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  addNewBook(myLibrary);
+})
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -10,10 +19,10 @@ Book.prototype.info = function () {
   return `${this.title} by ${this.author}, ${this.pages} pages, ${
     this.read ? "already read" : "not read yet"
   }`;
-}
+};
 
 function addBookToLibrary(array) {
-  array.forEach(book => {
+  array.forEach((book) => {
     const bookElement = document.createElement("div");
     const bookInfo = document.createElement("p");
 
@@ -22,4 +31,30 @@ function addBookToLibrary(array) {
 
     document.querySelector(".main").appendChild(bookElement);
   });
+}
+
+function toggleForm() {
+  const form = document.querySelector(".book-form");
+  form.classList.toggle("hidden");
+}
+
+function addNewBook(array) {
+  const newBook = new Book(
+    document.getElementById("title").value,
+    document.getElementById("author").value,
+    document.getElementById("pages").value,
+    document.getElementById("read").checked
+  );
+
+  document.getElementById("title").value = "";
+  document.getElementById("author").value = "";
+  document.getElementById("pages").value = "";
+  document.getElementById("read").checked = false;
+
+  array.push(newBook);
+
+  document.querySelector(".main").innerHTML = "";
+  addBookToLibrary(myLibrary);
+
+  toggleForm();
 }
